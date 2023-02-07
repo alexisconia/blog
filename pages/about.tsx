e8786d5334c466e28a39f4aebba97ab3eba8d418
Alexis CONIA <alexis.conia@live.fr>
2023-02-07 18:34:58 +0100

    Add about text sample

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,20 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <p>
+        Hi Im Alexis CONIA. Im software engineer. I started my company 10 years
+        ago to develop software on top of Microsoft 365 Platform.
+      </p>
+      <p>
+        Collaboration Software like realtime whiteboard, Videoconferencing
+        collaboration platform, and as background subject Teams Apps Platform.
+      </p>
+      <p>
+        As startup, you have to develop your swiss knife and explore lots of
+        aspects of software development. This experience leads me to work on all
+        the aspects of software development: UI / UX, Architecture, Development,
+        Deployment, Security, Devops, Monitoring and Supervision.
+      </p>
+    </div>
+  );
 }
